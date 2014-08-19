4764015ebf0c317275453518b87099bb0b45d3a2
JLuisOrtiz <jslsortiz428@gmail.com>
2014-08-19 14:36:15 -0600

    se esta haciendo pruebas

--- suma2/src/suma2/Suma2.java
+++ suma2/src/suma2/Suma2.java
@@ -13,6 +13,7 @@ public class Suma2 {
     /**
      * @param args the command line arguments
      */
+    public void sumar(){}
     public static void main(String[] args) {
         // TODO code application logic here
     }
