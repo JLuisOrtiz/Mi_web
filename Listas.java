3ea127765c289258e7fc05af17044002e1da0ecd
JLuisOrtiz <jslsortiz428@gmail.com>
2014-08-19 12:16:13 -0600

    se modifico la listas

---
+++ Listas.java
@@ -0,0 +1,24 @@
+
+package listas;
+
+
+public class Listas {
+
+ 
+    public static void main(String[] args) {
+        arraylist a=new arraylist(6);
+        a.agregarele(4);
+        a.agregarele(5);
+        a.agregarele(10);
+        a.verficalistallena();
+        a.inserteleposicion(5, 7);
+        a.imprimir();
+        
+        
+        
+      
+       
+        
+       
+    }
+}
